40bc894d56b91b3ac2bf54836ae133741e2de25c
Albin Annell <alan4480@rackham2.uppmax.uu.se>
2025-04-22 15:02:15 +0200

    quast of spades assembly

---
+++ code/04_assembly_evaluation_scripts/03_quast_assembly_evaluation_illumina_nanopore.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 00:40:00
+#SBATCH -J quast_pacbio
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user albin.annell.4480@student.uu.se
+#SBATCH --output=/home/alan4480/genome_analysis/Genome_Analysis/analyses/03_assembly_evaluation/03_quast_illumina_nanopore/quast.%j.out
+
+# Load modules
+module load bioinfo-tools
+module load quast
+
+# Define variables
+GENOME_SIZE="3m"
+ASSEMBLY="/home/alan4480/genome_analysis/Genome_Analysis/analyses/02_genome_assembly/02_illumina_nanopore_assembly/contigs.fasta"  
+OUTPUT_DIR="/home/alan4480/genome_analysis/Genome_Analysis/analyses/03_assembly_evaluation/03_quast_illumina_nanopore"
+
+#Create output directory if it does not exist
+mkdir -p $OUTPUT_DIR
+
+# Run QUAST
+quast.py $ASSEMBLY -o $OUTPUT_DIR --gene-finding
+
+echo "QUAST evaluation completed!"
+
+
