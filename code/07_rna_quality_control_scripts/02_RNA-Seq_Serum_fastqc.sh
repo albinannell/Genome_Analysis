b8d7143cad9839851c80d52712c27a168cf8173a
Albin Annell <alan4480@rackham3.uppmax.uu.se>
2025-04-25 16:49:55 +0200

    RNA fastqc and preprocessing

---
+++ code/07_rna_quality_control_scripts/02_RNA-Seq_Serum_fastqc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:30:00
+#SBATCH -J fastqc_rna_qc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user albin.annell.4480@student.uu.se
+#SBATCH --output=/home/alan4480/genome_analysis/Genome_Analysis/analyses/05_RNA/01_qc/01_qc_RNA-Seq_Serum/fastqc.%j.out
+
+# Load modules
+module load bioinfo-tools
+module load FastQC
+
+# Define variables
+INPUT_FILES="/home/alan4480/genome_analysis/Genome_Analysis/data/raw_data/transcriptomics/RNA-Seq_Serum/trim_paired*"  
+OUTPUT_DIR="/home/alan4480/genome_analysis/Genome_Analysis/analyses/05_RNA/01_qc/01_qc_RNA-Seq_Serum"
+
+# Create output directory if it doesn't exist
+mkdir -p $OUTPUT_DIR
+
+# Run FastQC on all input files
+fastqc -t 2 -o $OUTPUT_DIR $INPUT_FILES
+
+echo "RNA quality control for Serum completed!"
+
---
+++ code/08_rna_preprocessing_scripts/01_rna_trimmomatic_trimming.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 10:00:00
+#SBATCH -J trimmomatic_trim
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user albin.annell.4480@student.uu.se
+#SBATCH --output=/home/alan4480/genome_analysis/Genome_Analysis/analyses/05_RNA/02_trimming/01_trimming_RNA-Seq_BH_and_Serum/trimmomatic.%j.out
+
+# Load modules
+module load bioinfo-tools
+module load trimmomatic/0.39
+
+# Define variables
+RAW_DIR="/home/alan4480/genome_analysis/Genome_Analysis/data/raw_data/transcriptomics"
+OUTPUT_DIR="/home/alan4480/genome_analysis/Genome_Analysis/analyses/05_RNA/02_trimming/01_trimming_RNA-Seq_BH_and_Serum"
+ADAPTERS="/sw/apps/bioinfo/trimmomatic/0.39/rackham/adapters/TruSeq3-PE.fa"
+
+# Input files
+INPUT_FILES="$RAW_DIR/RNA-Seq_BH/trim_paired*"
+
+# Output files
+mkdir -p $OUTPUT_DIR
+
+# Run Trimmomatic for all input files
+for R1 in $INPUT_FILES; do
+    R2="${R1/_1/_2}"  # assuming paired-end reads, change if necessary
+    OUT_P1="$OUTPUT_DIR/$(basename $R1 .gz)_trimmed_R1_paired.fastq.gz"
+    OUT_UP1="$OUTPUT_DIR/$(basename $R1 .gz)_trimmed_R1_unpaired.fastq.gz"
+    OUT_P2="$OUTPUT_DIR/$(basename $R2 .gz)_trimmed_R2_paired.fastq.gz"
+    OUT_UP2="$OUTPUT_DIR/$(basename $R2 .gz)_trimmed_R2_unpaired.fastq.gz"
+
+    # Run Trimmomatic
+    trimmomatic PE -threads 2 -phred33 \
+        $R1 $R2 \
+        $OUT_P1 $OUT_UP1 \
+        $OUT_P2 $OUT_UP2 \
+        ILLUMINACLIP:$ADAPTERS:2:30:10 \
+        LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+done
+
+echo "Trimming completed!"
+
